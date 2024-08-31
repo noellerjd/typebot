a2fe38e1e90b866d8397a2a95dc48446f198ade7
noellerjd <noellerninja@gmail.com>
2024-08-31 10:59:48 -0600

    seperated the arrays from the main py file

--- bot.py
+++ bot.py
@@ -4,6 +4,13 @@ import random
 import discord
 from dotenv import load_dotenv
 
+from type_generation import type_people
+from type_generation import type_thing
+from type_generation import type_upgrade
+from type_generation import type_responses
+from type_generation import brownie_responses
+
+
 # Create an Intents object with the intents you want to enable
 intents = discord.Intents.default()
 intents.message_content = True  # Enable the message_content intent if you want to listen to messages
@@ -32,25 +39,7 @@ async def on_message(message):
         await message.channel.send(response)
 
 # Return a response after being pinged
-
-#Old responses -- to be added in as an additional option over procedural generation if wanted. 
-    # type_responses = [
-    #     'Amen', 'Godbless', 'I invented transistors', 'Idris Elba feet picture high resolution Creative Commons license', 'Shroud gaming setup no socks with RGB lighting Commercial License free download 32gb ram upgrade', 'Markiplier free smiling wholesome footage no socks red background', 'How to train your dragon toothless kickflip on scooter real footage no cgi', 'Hello :)', 'Stung by wasp on big toe 8k resolution', '😈', '🐝', , 'Sasquach gaming 420 1080p 2gb ram fail complation 2029', 'I eated a bug :)',
-
-    # ]
-
-#Procedural Generation
-
-    type_people = [
-        'Idris Elba', 'Tyler "Ninja" Blevins', 'Bill Gates', 'Samsung 860 EVO 2.5" SSD SATA III 500GB', 'Japanese Toilet Assistant from Cars 2', 'Barry B. Benson', 'Markiplier', 'Shroud', 'Thomas Holland', 'Remy the Rat from Remy\'s Ratatouille Adventure', 'Shadow the Hedge Hog', 'Will Smith in Bad Boys 2', 'Jamie Lee Curtis from Freaky Friday', 'The Dog from Dog with a blog', '<@382370044144779265>'
-    ]
-
-    type_thing = [
-        'kidney stone irl very shaky footage lots of screaming', 'virus 2024 pandemic angry scene', 'says terrible things about a very nice old man', 'calls me papa while I try to run away', 'wholesome appendicitis moment', 'suggests only they can use the sink in the restroom chaos ensues', 'big diaper diaper baby waddling diaper baby has no fingers', 'Mukbang Play Doh Modeling Compund 4.0oz x 4 pack $6.99 at walgreens', 'no shoes animated GIF stinks to high heavens', 'praying to the wrong god complation blasphemy edition', 'goes to hell aftermath god laughs', 'gaming for 3 hours straight aftermath god reaction gone wrong'
-    ]
-    type_upgrade = [
-        'r/wholesome free video download no surveys','kickstarter video flexible funding', 'vimeo.com 2026 trailer leak 69/100 on metacritic', 'youtube.ru putin approved version', 'my dad is yelling from the other room but I can\'t tell if hes mad at me or if he\'s eating spaghetti', 'recalled 2016 Tristar AquaRug shower rugs because suction cups on the underside of the rugs fail and cause the one thing that it was supposed to prevent', 'random guy yelling at you in 2013 made you feel bad', 'justice finally enacted death penalty confirmed', 'gets away with stealing gum from the cash register I\'m mad about it alibaba.freak', 'trapped inside my computer, wont let me open microsoft edge'
-    ]
+#Procedural Generation moved to seprate files
 
     if message.content.lower() in ['type', '<@382370044144779265>']:
         def make_sentence():
@@ -63,7 +52,11 @@ async def on_message(message):
         def upgrade():
             return random.choice(type_upgrade)
         
-        response = make_sentence()
+        if random.random() < 0.333:
+            response = random.choice(type_responses)
+        else:
+            response = make_sentence()
+
         await message.channel.send(response)
 
 
@@ -74,12 +67,8 @@ async def on_message(message):
         response = 'https://i.imgur.com/H8u43Up.png'
         await message.channel.send(response)
 
-# A list of responsed that has a 50% chance to send after brownie sends something that isn't a link or gif in the discord chat.
+# Uses a list of responses that has a 25% chance to send after brownie sends something that isn't a link or gif in the discord chat.
 
-    brownie_responses = [
-        'Wow that was very rude what you just said.', 'who asked?', 'Not very mowful not very demure', 'SMH 💀', 'Do you kiss ur cat mom with that mouth?', 'Dylan is a known cheater who boosts his meep scores.', 'I disagree with what Brownie just said.', 'Bruh', 'I actually agree with what brownie said here.', 'Brownie actually cooked with this one.', 
-        'wow, you really just said \"{browniemessage}\"?', 'Bro really just said \"{browniemessage}\".', '\"{browniemessage}\"🤓'
-    ]
     if message.author.id == 1137831321599746158:
         if 'https://' in message.content:
             return
---
+++ type_generation.py
@@ -0,0 +1,37 @@
+type_people = [
+    'Idris Elba', 'Tyler "Ninja" Blevins', 'Bill Gates', 'Samsung 860 EVO 2.5" SSD SATA III 500GB', 
+    'Japanese Toilet Assistant from Cars 2', 'Barry B. Benson', 'Markiplier', 'Shroud', 'Thomas Holland', 
+    'Remy the Rat from Remy\'s Ratatouille Adventure', 'Shadow the Hedge Hog', 'Will Smith in Bad Boys 2', 
+    'Jamie Lee Curtis from Freaky Friday', 'The Dog from Dog with a blog', '<@382370044144779265>'
+]
+
+type_thing = [
+    'kidney stone irl very shaky footage lots of screaming', 'virus 2024 pandemic angry scene', 
+    'says terrible things about a very nice old man', 'calls me papa while I try to run away', 
+    'wholesome appendicitis moment', 'suggests only they can use the sink in the restroom chaos ensues', 
+    'big diaper diaper baby waddling diaper baby has no fingers', 
+    'Mukbang Play Doh Modeling Compund 4.0oz x 4 pack $6.99 at walgreens', 
+    'no shoes animated GIF stinks to high heavens', 'praying to the wrong god complation blasphemy edition', 
+    'goes to hell aftermath god laughs', 'gaming for 3 hours straight aftermath god reaction gone wrong'
+]
+
+type_upgrade = [
+    'r/wholesome free video download no surveys','kickstarter video flexible funding', 
+    'vimeo.com 2026 trailer leak 69/100 on metacritic', 'youtube.ru putin approved version', 
+    'my dad is yelling from the other room but I can\'t tell if hes mad at me or if he\'s eating spaghetti', 
+    'recalled 2016 Tristar AquaRug shower rugs because suction cups on the underside of the rugs fail and cause the one thing that it was supposed to prevent', 
+    'random guy yelling at you in 2013 made you feel bad', 'justice finally enacted death penalty confirmed', 
+    'gets away with stealing gum from the cash register I\'m mad about it alibaba.freak', 
+    'trapped inside my computer, wont let me open microsoft edge'
+]
+
+type_responses = [
+    'Amen', 'Godbless', 'I invented transistors', 'Hello :)', 
+    'Stung by wasp on big toe 8k resolution', '😈', '🐝', 'I eated a bug :)',
+    'I eated a ghost :)', 'Something is wrong 😳', '<@382370044144779265> 💪😎', 'Real gamers go to https://www.twitch.tv/bungoh and make sure to unfollow and report for Off Twitch Behavior :saluting_face:'
+]
+
+brownie_responses = [
+        'Wow that was very rude what you just said.', 'who asked?', 'Not very mowful not very demure', 'SMH 💀', 'Do you kiss ur cat mom with that mouth?', 'Dylan is a known cheater who boosts his meep scores.', 'I disagree with what Brownie just said.', 'Bruh', 'I actually agree with what brownie said here.', 'Brownie actually cooked with this one.', 
+        'wow, you really just said \"{browniemessage}\"?', 'Bro really just said \"{browniemessage}\".', '\"{browniemessage}\"🤓'
+]
