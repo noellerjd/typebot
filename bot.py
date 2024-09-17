4abc5e4861fb97f8b1a2722ca9b3a06a001550ee
noellerjd <noellerninja@gmail.com>
2024-09-17 17:02:07 -0600

    Updated for use within other servers

--- bot.py
+++ bot.py
@@ -10,10 +10,6 @@ from dotenv import load_dotenv
 
 from type_generation import type_people, type_thing, type_upgrade, type_responses, brownie_responses, rrisky_responses, david_responses, random_compliments
 
-# from dnd_data import races
-from dnd_data import races, classes, backgrounds
-races_list = ", ".join(races)
-
 # Create an Intents object with the intents you want to enable
 intents = discord.Intents.default()
 intents.message_content = True  # Enable the message_content intent if you want to listen to messages
@@ -25,6 +21,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 restricted_channel_id = 1280670129939681357
+meep_server_id = 1129622683546554479
 
 @bot.event
 async def on_ready():
@@ -70,6 +67,50 @@ async def roll(interaction: discord.Interaction, dice: str):
     # Edit the original message with the result
     await interaction.edit_original_response(content=result)
 
+# Secret rolls for the DM
+@bot.tree.command(name="dmroll")
+@app_commands.describe(dice="Roll dice using the format 'XdY+Z'")
+async def roll(interaction: discord.Interaction, dice: str):
+    # Acknowledge the interaction immediately with a placeholder message
+    await interaction.response.send_message('Rolling...', ephemeral=True)
+
+    await asyncio.sleep(1)
+
+    dice_pattern = re.compile(r'(\d*)d(\d+)([+-]\d+)?')
+    match = dice_pattern.match(dice)
+
+    if not match:
+        await interaction.edit_original_response(content="Invalid dice format. Roll dice using the format 'XdY+Z'.", ephemeral=True)
+        return
+
+    num_dice = int(match.group(1)) if match.group(1) else 1
+    dice_size = int(match.group(2))
+    dice_modifier = int(match.group(3)) if match.group(3) else 0
+
+    rolls = [random.randint(1, dice_size) for _ in range(num_dice)]
+    total = sum(rolls) + dice_modifier
+
+    roll_details = f"Rolls: {', '.join(map(str, rolls))}"
+
+    if dice_modifier:
+        roll_details += f" with modifier {dice_modifier}"
+
+    result = f"{interaction.user.mention} rolled {num_dice}d{dice_size}{match.group(3) or ''}: {roll_details}. Total: {total}"
+
+    # Edit the original message with the result
+    await interaction.edit_original_response(content=result)
+
+# Whisper command to tell only one person something
+@bot.tree.command(name="whisper")
+@app_commands.describe(target="The player to send a secret message to", message="The secret message to send")
+async def whisper(interaction: discord.Interaction, target: discord.User, message: str):
+    # Send a secret message to the target player
+    await target.send(f"{interaction.user.mention} whispers: {message}")
+    # Let the whisperer know the message was sent
+    await interaction.response.send_message(f"Whisper sent to {target.mention}", ephemeral=True)
+
+
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -77,6 +118,12 @@ async def on_message(message):
     if message.channel.id == restricted_channel_id:
         return    
 
+
+
+# Meep Specific Commands
+    # Do not return anything message is if not in the meep server
+    if message.guild.id != meep_server_id:
+        return
     # Return a response after pinging type
     if message.content.lower() in ['type', '<@382370044144779265>']:
         def make_sentence():
@@ -104,7 +151,6 @@ async def on_message(message):
     if any(trigger in message.content.lower() for trigger in ['david', 'flare', '<@125063361196064768>']):
         response = random.choice(david_responses)
         await message.channel.send(response)
-
     # Return a picture of dylans foot after someone says toe
     if message.content == 'toe': 
         if random.random() > 0.9:
@@ -112,12 +158,10 @@ async def on_message(message):
         else:
             response = 'https://i.imgur.com/H8u43Up.png'
         await message.channel.send(response)
-
     # Return a message if json is mentioned
     if any(trigger in message.content.lower() for trigger in ['json']):
         response = f'<@{382370044144779265}> has been summoned.'
         await message.channel.send(response)
-
     # Uses a list of responses that has a 25% chance to send after brownie sends something that isn't a link or gif in the discord chat.
     if message.author.id == 1137831321599746158:
 
